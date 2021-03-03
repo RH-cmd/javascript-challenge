065d83d909644a9fde06afb840292c0b3a606ab1
Ronjini Hassan <ronjinihassan@ronjinis-mbp.home>
2021-03-02 22:42:44 -0500

    Create the table and append data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,49 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// View the data from the data.js file 
+console.log(data);
+
+// Reference the table body 
+var tbody = d3.select("tbody");
+
+// Create a function that will use the data to populate the table
+function UFOtable () {tableData.map (data => {
+    // Create a new row
+    var newRow = tbody.append('tr');
+
+    // Add new rows of data for each UFO sighting
+    newRow.append('td').text(data.datetime);
+    newRow.append('td').text(data.city);
+    newRow.append('td').text(data.state);
+    newRow.append('td').text(data.country);
+    newRow.append('td').text(data.shape);
+    newRow.append('td').text(data.durationMinutes);
+    newRow.append('td').text(data.comments);
+    });
+}
+
+// Show the table
+UFOtable();
+
+// Add event listener for button on Filter Search
+var inputFilter = d3.select('#filter-btn');
+
+// Create a function to take the input from the Filter Search and recreate table
+inputFilter.on('click', function () {
+
+    // Stop page from refreshing
+    d3.event.preventDefault();
+
+    // Select the user input value from Filter Search 
+    var input = d3.select('#datetime').node().value;
+    console.log(input);
+
+    // Filter the data 
+    
+
+
+
+}
+)
+
